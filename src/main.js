a024613b520e656e845b5561bd56315ed57637ff
eeeeea <eeeeea@gmail.com>
2025-03-12 22:45:14 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {Quasar} from 'quasar'
 
 
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
 
 const app = createApp(App);
 app.use(ElementPlus);
+app.use(Quasar, {
+    plugins: {}, // 可以按需添加 Quasar 插件，如 Notify、Dialog 等
+  });
 app.mount('#app');
 
